d2b7a9fb8a506b60d0176c773ed40f8ce7ba4ac2
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-08-02 14:40:57 +0800

    feat:工厂函数的封装

---
+++ src/icon/index.tsx
@@ -0,0 +1,53 @@
+import React, { forwardRef, type PropsWithChildren } from 'react'
+import cs from 'classnames'
+
+import './index.scss'
+
+type BaseIconProps = {
+  className?: string
+  style?: React.CSSProperties
+  size?: string | string[]
+  spin?: boolean
+}
+
+export type IconProps = BaseIconProps &
+  Omit<React.SVGAttributes<SVGElement>, keyof BaseIconProps>
+
+const getSize = (size: IconProps['size']) => {
+  if (Array.isArray(size) && size.length === 2) {
+    return size as string[]
+  }
+  const width = (size as string) || '1em'
+  const height = (size as string) || '1em'
+
+  return [width, height]
+}
+
+export const Icon = forwardRef<SVGSVGElement, PropsWithChildren<IconProps>>(
+  (props, ref) => {
+    const { style, className, spin, size = '1em', children, ...rest } = props
+
+    const [width, height] = getSize(size)
+
+    const cn = cs(
+      'icon',
+      {
+        icon_spin: spin,
+      },
+      className
+    )
+
+    return (
+      <svg
+        ref={ref}
+        style={style}
+        fill='currentColor'
+        className={cn}
+        width={width}
+        height={height}
+        {...rest}>
+        {children}
+      </svg>
+    )
+  }
+)
