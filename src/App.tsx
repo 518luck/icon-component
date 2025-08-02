86eebeff1723fa3fc6c54611cd8d9a77c27c74d1
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-08-02 14:45:05 +0800

    feat:测试svg图标

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { IconAdd } from './icon/icons/iconAdd'
+import { IconEmail } from './icon/icons/IconEmail'
+
 function App() {
-  return <>this is app</>
+  return (
+    <div style={{ padding: '50px' }}>
+      <IconAdd size='40px'></IconAdd>
+      <IconEmail spin></IconEmail>
+      <IconEmail style={{ color: 'blue', fontSize: '50px' }}></IconEmail>
+    </div>
+  )
 }
 
 export default App
